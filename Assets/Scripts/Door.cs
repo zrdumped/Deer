5edc8b9119a9985f1820f5973bbcbc2107a9ded0
Mr.Melon <nmgzhangran@163.com>
2018-09-19 16:03:39 +0800

    Tiny updates on Door and Corridor scene

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -6,23 +6,34 @@ using UnityEngine.SceneManagement;
 
 public class Door : MonoBehaviour {
 
-    public bool active = false;
-    public string sceneMessage = "Scene Message";
+    private bool active = false;
+    [Header("Enter")]
+    public GameObject placeHolder;
+    [Header("Scene")]
+    public string currentScene = "Scene Message";
+    public string otherScene = "Scene Message";
+    [Header("Interaction")]
     public string hintMessage = "Hint Message";
-    public string leadTo = "";
     public KeyCode enterKey = KeyCode.E;
 
 	// Use this for initialization
 	void Start () {
-        GameObject.Find("HintMessage").GetComponent<Text>().text = sceneMessage;
+        //GameObject.Find("HintMessage").GetComponent<Text>().text = hintMessage;
+        active = false;
+        if(PlayerPrefs.GetString("from") == otherScene)
+        {
+            PlayerPrefs.SetString("from", "");
+            GameObject.Find("Liam").transform.position = placeHolder.transform.position;
+            GameObject.Find("Liam").transform.rotation = placeHolder.transform.rotation;
+        }
     }
 	
 	// Update is called once per frame
 	void Update () {
         if (active && Input.GetKeyDown(enterKey))
         {
-            if (leadTo != "")
-                SceneManager.LoadScene(leadTo);
+            PlayerPrefs.SetString("from", currentScene);
+            SceneManager.LoadScene(otherScene);
         }
 	}
 
@@ -35,6 +46,6 @@ public class Door : MonoBehaviour {
     private void OnTriggerExit(Collider other)
     {
         active = false;
-        GameObject.Find("HintMessage").GetComponent<Text>().text = sceneMessage;
+        GameObject.Find("HintMessage").GetComponent<Text>().text = "";
     }
 }
