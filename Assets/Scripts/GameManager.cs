ced64ae5f8f06236a1d9a9131b50a62eee073b59
rebas777 <154587260@qq.com>
2018-10-04 21:36:25 +0800

    add prologue scene

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour {
+
+    private int curChap; // Which chapter 
+    private int curScene; // Which scene of current chapter 
+    private bool isPause = false;
+
+
+	// Use this for initialization
+	void Start () {
+        Load("1_0_Prologue");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PauseGame() {
+
+    }
+
+    public void ResumeGame() {
+
+    }
+
+    // Enable player to control the character
+    public void StartCtrl() {
+
+    }
+
+    // Disable player to control the character
+    public void StopCtrl() {
+
+    }
+
+    public void GotoMainPanel() {
+
+    }
+
+    // Start a new game process and delete the record which is being stored
+    public void NewGame() {
+
+    }
+
+    public void ExitGame() {
+
+    }
+
+    // Save the player's current game process(ONLY ONE record is supported)
+    public void SaveProcess() {
+
+    }
+
+    public void LoadProcess() {
+
+    }
+
+    public void Load(string sceneName) {
+        if(!SceneManager.GetSceneByName(sceneName).isLoaded)
+            SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+    }
+
+    public void Unload(string sceneName) {
+        if(SceneManager.GetSceneByName(sceneName).isLoaded)
+            SceneManager.UnloadSceneAsync(sceneName);
+    }
+
+    public void Switch2Scene(int targetChap, int targetScene) {
+
+    }
+}
---
+++ Assets/Scripts/Post-Processing/ScreenTone.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenTone : PostEffectsBase {
+
+    public Shader screenToneShader;
+    private Material screenToneMaterial = null;
+    public Material material {
+        get {
+            screenToneMaterial = CheckShaderAndCreateMaterial(screenToneShader, screenToneMaterial);
+            return screenToneMaterial;
+        }
+    }
+
+    public Color color;
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest) {
+
+    }
+}
