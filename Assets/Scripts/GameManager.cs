316ffa769346859db31d6dcffece7a2b6c30992a
rebas777 <154587260@qq.com>
2018-10-06 18:06:17 +0800

    add scene switch control

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,17 +9,36 @@ public class GameManager : MonoBehaviour {
     private int curChap; // Which chapter 
     private int curScene; // Which scene of current chapter 
     private bool isPause = false;
+    private string curSceneStr = "";
 
 
 	// Use this for initialization
 	void Start () {
-        Load("1_0_Prologue");
+        GotoMainPanel();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        // For Test ONLY
+        if(Input.GetKeyDown(KeyCode.H)) {
+            Switch2Scene(0, 0);
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha0)) {
+            Switch2Scene(1, 0);
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha1)) {
+            Switch2Scene(1, 1);
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha2)) {
+            Switch2Scene(1, 2);
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha3)) {
+            Switch2Scene(1, 3);
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha4)) {
+            Switch2Scene(1, 4);
+        }
+    }
 
     public void PauseGame() {
 
@@ -40,12 +59,15 @@ public class GameManager : MonoBehaviour {
     }
 
     public void GotoMainPanel() {
-
+        Switch2Scene(0, 0);
     }
 
     // Start a new game process and delete the record which is being stored
     public void NewGame() {
+        // TODO: clear previously saved record
 
+
+        Switch2Scene(1, 0);
     }
 
     public void ExitGame() {
@@ -72,6 +94,11 @@ public class GameManager : MonoBehaviour {
     }
 
     public void Switch2Scene(int targetChap, int targetScene) {
-
+        string sceneName = "" + targetChap + "_" + targetScene;
+        if(curSceneStr != "") {
+            Unload(curSceneStr);
+        }
+        Load(sceneName);
+        curSceneStr = sceneName;
     }
 }
