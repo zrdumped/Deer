4cd237901c367f42c1d2e0611ff7a124beb7cbf1
Mr.Melon <nmgzhangran@163.com>
2018-10-08 18:40:52 +0800

    Puzzle in progress

---
+++ Assets/MyEditor/KeyEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+[CustomEditor(typeof(Key))]
+public class NewBehaviourScript2 : Editor
+{
+    private SerializedObject key;
+    private SerializedProperty pattern;
+    void OnEnable()
+    {
+        key = new SerializedObject(target);
+        pattern = key.FindProperty("pattern");
+    }
+    public override void OnInspectorGUI()
+    {
+        key.Update();
+        //for(int i = 0; i < patternNum; i++)
+        EditorGUILayout.PropertyField(pattern, true);
+        key.ApplyModifiedProperties();
+    }
+}
