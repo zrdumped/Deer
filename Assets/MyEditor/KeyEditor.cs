12a470df9c2eecf5eb2f3a99f6172cd243173ebb
Mr.Melon <nmgzhangran@163.com>
2018-10-09 12:59:35 +0800

    Hints Ball Logic Completed

--- Assets/MyEditor/KeyEditor.cs
+++ Assets/MyEditor/KeyEditor.cs
@@ -1,10 +1,15 @@
 ﻿using UnityEngine;
 using UnityEditor;
-[CustomEditor(typeof(Key))]
+[CustomEditor(typeof(Pattern))]
 public class NewBehaviourScript2 : Editor
 {
     private SerializedObject key;
     private SerializedProperty pattern;
+    //private SerializedProperty minScale;
+    //private SerializedProperty maxScale;
+    //private SerializedProperty pitchLength;
+    //private SerializedProperty intermissionLength, changeFrames;
+
     void OnEnable()
     {
         key = new SerializedObject(target);
@@ -13,7 +18,15 @@ public class NewBehaviourScript2 : Editor
     public override void OnInspectorGUI()
     {
         key.Update();
-        //for(int i = 0; i < patternNum; i++)
+        EditorGUILayout.PropertyField(key.FindProperty("minScale"));
+        EditorGUILayout.PropertyField(key.FindProperty("maxScale"));
+        EditorGUILayout.PropertyField(key.FindProperty("pitchLength"));
+        EditorGUILayout.PropertyField(key.FindProperty("intermissionLength"));
+        EditorGUILayout.PropertyField(key.FindProperty("changeFrames"));
+        // EditorGUILayout.PropertyField(minScale);
+        //EditorGUILayout.PropertyField(maxScale);
+        //EditorGUILayout.PropertyField(pitchLength);
+        //EditorGUILayout.PropertyField(intermissionLength);
         EditorGUILayout.PropertyField(pattern, true);
         key.ApplyModifiedProperties();
     }
---
+++ Assets/Scripts/Pattern.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pattern : MonoBehaviour {
+
+
+    public enum PitchType { Ultralow, Low, Medium, High, Ultrahigh};
+    [Header("Pattern Control")]
+    public int minScale = 1;
+    public int maxScale = 10;
+    public int pitchLength = 100;  //seconds
+    public int intermissionLength = 2;   //multiple of pitch length
+    public int changeFrames = 20; 
+    public PitchType[] pattern;
+
+
+    private int pitchID = 0;
+    private int patternLength;
+    private float[] patternScale = new float[5];
+    private bool onShow = false;
+    private float scaleChangeStep = 0;
+    private int changedFrame = 0;
+
+	// Use this for initialization
+	void Start () {
+        patternLength = pattern.Length;
+        float step = ((float)maxScale - (float)minScale) / 4.0f;
+        patternScale[0] = (float)minScale;
+        patternScale[1] = (float)minScale + step * 1;
+        patternScale[2] = (float)minScale + step * 2;
+        patternScale[3] = (float)minScale + step * 3;
+        patternScale[4] = (float)maxScale;
+        this.transform.localScale = new Vector3(0, 0, 0);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            //Debug.Log("E" + this.name);
+            if (onShow)
+            {
+                StopCoroutine("showHint");
+                onShow = false;
+                this.transform.localScale = new Vector3(0, 0, 0);
+                pitchID = 0;
+                scaleChangeStep = 0;
+                changedFrame = 0;
+                Debug.Log("Stop");
+            }
+            else
+            {
+                StartCoroutine("showHint");
+                onShow = true;
+                Debug.Log("Start");
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (scaleChangeStep != 0 && onShow)
+        {
+            this.transform.localScale += new Vector3(scaleChangeStep, scaleChangeStep, scaleChangeStep);
+            changedFrame++;
+            if (changedFrame == changeFrames)
+            {
+                scaleChangeStep = 0;
+                changedFrame = 0;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    IEnumerator showHint() {
+        while (true)
+        {
+            float targetScale;
+            if (pitchID >= 0)
+            {
+                targetScale = patternScale[(int)pattern[pitchID]];
+                //this.transform.localScale = new Vector3(curScale, curScale, curScale);
+            }
+            else
+            {
+                targetScale = 0;
+                //this.transform.localScale = new Vector3(0, 0, 0);
+            }
+            //SetTargetScale(targetScale);
+            scaleChangeStep = (targetScale - this.transform.localScale.x) / (float)changeFrames;
+            Debug.Log(scaleChangeStep);
+            pitchID++;
+            if (pitchID == pattern.Length)
+                pitchID = -intermissionLength;
+            yield return new WaitForSeconds(pitchLength);
+        }
+    }
+}
