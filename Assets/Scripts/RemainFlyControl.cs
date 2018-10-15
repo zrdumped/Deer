652708fc0135db132d49f595594b7611a36f7cd1
rebas777 <154587260@qq.com>
2018-10-15 17:00:29 +0800

    add Deer Remain Timeline

---
+++ Assets/Scripts/RemainFlyControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemainFlyControl : MonoBehaviour {
+
+    public GameObject target;
+    public float lerpVal = 0;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 curPos = transform.position;
+        Vector3 tmpPos = Vector3.Lerp(curPos, target.transform.position, lerpVal);
+        transform.position = tmpPos;
+	}
+}
