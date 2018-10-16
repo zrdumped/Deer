d9b4ed669cbb97f7ba02ce62bae7955a4d49705d
Mr.Melon <nmgzhangran@163.com>
2018-10-16 18:36:18 +0800

    Add Lightball & light control (parameters remain to be modified)

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -9,8 +9,16 @@ public class Character : MonoBehaviour {
     // Use this for initialization
     [Header("State Config")]
     public bool controlByVoice = false;
-    public float LightBallChangeRate = 0.01f;
-    private float currentLightBallChangeRate;
+    //public float LightBallChangeRate = 0.01f;
+    //public float PointLightRangeChangeRate = 1.0f;
+    //public float PointLightIntensityChangeRate = 1.0f;
+    //public float maxLightBallScale = 0.15f;
+    //private float currentLightBallChangeRate;
+    public float maxLightBallScale = 0.15f;
+    public float maxLightIntensity = 2;
+    public float maxLightRange = 9;
+    [Header("Control By Keyboard")]
+    public float changeTimes = 10;
     [Header("Keyboard Control Config")]
     public KeyCode inflateKey = KeyCode.Z;
     public KeyCode deflateKey = KeyCode.X;
@@ -18,10 +26,27 @@ public class Character : MonoBehaviour {
     //private enum toolState { Inflate, Deflate, Inactive };
     //private toolState state = toolState.Inactive;
     private GameObject lightBall;
+    private GameObject lightSphere;
+
+    private float LightBallScaleRate;
+    private float LightIntensityRate;
+    private float LightRangeRate;
 
     void Start () {
         lightBall = this.transform.Find("LightBall").gameObject;
-	}
+        lightBall.transform.localScale = new Vector3(0, 0, 0);
+
+        lightSphere = this.transform.Find("Point Light").gameObject;
+        lightSphere.GetComponent<Light>().range = 0;
+        lightSphere.GetComponent<Light>().intensity = 0;
+
+        if (!controlByVoice)
+        {
+            LightBallScaleRate = maxLightBallScale / changeTimes;
+            LightIntensityRate = maxLightIntensity / changeTimes;
+            LightRangeRate = maxLightRange / changeTimes;
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -30,11 +55,37 @@ public class Character : MonoBehaviour {
         if (!controlByVoice)
         {
             if (Input.GetKey(inflateKey))
-                lightBall.transform.localScale += new Vector3(LightBallChangeRate, LightBallChangeRate, LightBallChangeRate);
+            {
+                //Debug.Log(lightBall.transform.localScale.x + LightBallChangeRate);
+                //Debug.Log(maxLightBallScale);
+                if (lightBall.transform.localScale.x + LightBallScaleRate < maxLightBallScale)
+                {
+                    lightBall.transform.localScale += new Vector3(LightBallScaleRate, LightBallScaleRate, LightBallScaleRate);
+                    lightSphere.GetComponent<Light>().range += LightRangeRate;
+                    lightSphere.GetComponent<Light>().intensity += LightIntensityRate;
+                }
+            }
             else if (Input.GetKey(deflateKey))
-                lightBall.transform.localScale -= new Vector3(LightBallChangeRate, LightBallChangeRate, LightBallChangeRate);
+            {
+                if (lightBall.transform.localScale.x - LightBallScaleRate >= 0)
+                {
+                    lightBall.transform.localScale -= new Vector3(LightBallScaleRate, LightBallScaleRate, LightBallScaleRate);
+                    lightSphere.GetComponent<Light>().range -= LightRangeRate;
+                    lightSphere.GetComponent<Light>().intensity -= LightIntensityRate;
+                }
+                else
+                {
+                    lightBall.transform.localScale = new Vector3(0, 0, 0);
+                    lightSphere.GetComponent<Light>().range = 0;
+                    lightSphere.GetComponent<Light>().intensity = 0;
+                }
+            }
             else if (Input.GetKeyDown(disappearKey))
+            {
                 lightBall.transform.localScale = new Vector3(0, 0, 0);
+                lightSphere.GetComponent<Light>().range = 0;
+                lightSphere.GetComponent<Light>().intensity = 0;
+            }
         }
     }
 }
