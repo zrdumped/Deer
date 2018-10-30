96b02fb88390736be640a3df32155660af0b699c
Mr.Melon <nmgzhangran@163.com>
2018-10-30 18:43:05 +0800

    Adding activate order

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -9,6 +9,7 @@ public class Key : MonoBehaviour
 	[Header("VFX")]
 	//the vfx shown normally
 	public GameObject InactiveBall;
+    public GameObject InactiveBall_2;
 	//the vfx shown after pressing E
 	public GameObject HintBall;
 	//the vfx shown after all matching
@@ -16,12 +17,15 @@ public class Key : MonoBehaviour
 	//the vfxs shown after single matching
 	public GameObject AlertBall;
 	public GameObject ConfirmBall;
+    public GameObject WrongBall;
+    public GameObject signal;
 	public int DestroySeconds = 1;
 	[Header("Light Up Settings")]
 	public GameObject[] Lights;
 	//public float initEmissionScale = 1;
     public Material noLightMaterial;
 	public int OnLitFrames = 200;
+    public Key[] formerKeys;
 
     private ArrayList onLightMaterial;
     private float[] lightRangeSteps, lightIntensitySteps;
@@ -35,7 +39,9 @@ public class Key : MonoBehaviour
 	void Start()
 	{
 		InactiveBall.GetComponent<ParticleSystem>().Play(true);
-		ActiveBall.GetComponent<ParticleSystem>().Stop(true);
+        if (InactiveBall_2 != null)
+            InactiveBall_2.GetComponent<ParticleSystem>().Play(true);
+        ActiveBall.GetComponent<ParticleSystem>().Stop(true);
 		lightRangeSteps = new float[Lights.Length];
 		lightIntensitySteps = new float[Lights.Length];
         onLightMaterial = new ArrayList();
@@ -94,6 +100,11 @@ public class Key : MonoBehaviour
 	// Update is called once per frame
 	void FixedUpdate()
 	{
+        if(status == KeyStatus.Inactive && InactiveBall_2 != null)
+        {
+            InactiveBall.transform.RotateAround(this.transform.position, Vector3.up, 90 * Time.deltaTime);
+            InactiveBall_2.transform.RotateAround(this.transform.position, Vector3.up, 90 * Time.deltaTime);
+        }
 		if (curOnLitFrame < OnLitFrames)
 		{
 			curOnLitFrame++;
@@ -116,10 +127,13 @@ public class Key : MonoBehaviour
 		if (status == KeyStatus.Inactive)
 		{
 			InactiveBall.SetActive(false);
+            if(InactiveBall_2 != null)
+                InactiveBall_2.SetActive(false);
             HintBall.SetActive(true);
             this.GetComponentInChildren<Pattern>().enabled = true;
             targetMatchNum = this.GetComponentInChildren<Pattern>().pattern.Length;
             //walk on stage and disable moving
+            this.GetComponent<KeyCharacterController>().GotoKey();
             status = KeyStatus.OnShow;
 			//GetComponentInChildren<Pattern>().character = GetComponent<InputListener>().character;
 			return "Press E to stop. Match the spheres with your voice";
@@ -128,12 +142,12 @@ public class Key : MonoBehaviour
 		{
             HintBall.SetActive(false);
             InactiveBall.SetActive(true);
-            //InactiveBall.GetComponent<ParticleSystem>().Play(true);
-            //this.GetComponentInChildren<Pattern>().enabled = false;
-			//walk off stage and enable moving
-			status = KeyStatus.Inactive;
-			//GetComponentInChildren<Pattern>().character = null;
-			return "Press E to dispaly.";
+            if (InactiveBall_2 != null)
+                InactiveBall_2.SetActive(true);
+            status = KeyStatus.Inactive;
+            this.GetComponent<KeyCharacterController>().LeaveKey();
+            //GetComponentInChildren<Pattern>().character = null;
+            return "Press E to dispaly.";
 		}
 		else
 		{
@@ -141,9 +155,35 @@ public class Key : MonoBehaviour
 		}
 	}
 
-	public void activate()
+    public void activateWrong()
+    {
+        WrongBall.SetActive(true);
+        HintBall.SetActive(false);
+        StartCoroutine(WaitAndInactive(WrongBall, DestroySeconds));
+    }
+
+    public void activate()
 	{
-		Debug.Log("called activated");
+        if (formerKeys.Length == 1)
+        {
+            if (formerKeys[0].GetKeyStatus() != KeyStatus.Active)
+            {
+                GameObject.Find("HintMessage").GetComponent<Text>().text = "This should not be activated now";
+                activateWrong();
+                return;
+            }
+        }else if(formerKeys.Length == 2)
+        {
+            KeyStatus tmpStatus1 = formerKeys[0].GetKeyStatus();
+            KeyStatus tmpStatus2 = formerKeys[1].GetKeyStatus();
+            if(tmpStatus1 != KeyStatus.Active || tmpStatus2 != KeyStatus.Active)
+            {
+                GameObject.Find("HintMessage").GetComponent<Text>().text = "This should not be activated now";
+                activateWrong();
+                return;
+            }
+        }
+        Debug.Log("called activated");
 		this.GetComponentInChildren<Pattern>().enabled = false;
         HintBall.SetActive(false);
         status = KeyStatus.Active;
@@ -212,6 +252,19 @@ public class Key : MonoBehaviour
 		Destroy(obj);
 	}
 
+    IEnumerator WaitAndInactive(GameObject obj, int time)
+    {
+        Debug.Log("as");
+        yield return new WaitForSeconds(time);
+        //HintBall.transform.localScale = obj.transform.localScale;
+        Debug.Log("as");
+        obj.SetActive(false);
+        InactiveBall.SetActive(true);
+        if (InactiveBall_2 != null)
+            InactiveBall_2.SetActive(true);
+        status = KeyStatus.Inactive;
+    }
+
     public void MatchReset()
     {
         curMatchNum = 0;
@@ -220,5 +273,12 @@ public class Key : MonoBehaviour
     public KeyStatus GetKeyStatus()
     {
         return status;
+    }
+
+    public void Showline(GameObject target)
+    {
+        Waypoint t = target.GetComponent<Waypoint>();
+        signal.GetComponentInChildren<WaypointsHolder>().waypoints = new List<Waypoint> { t };
+        signal.GetComponentInChildren<WaypointMover>().enabled = true;
     }
 }
