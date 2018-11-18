e3bfc94c027e18e9a08aef8f5f0206ec61a44fc1
rebas777 <154587260@qq.com>
2018-11-18 17:14:41 +0800

    add timeline bridge

---
+++ Assets/Scripts/TimelineTrigger/BridgeTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class BridgeTrigger : MonoBehaviour {
+
+    public GameObject player;
+    public PlayableDirector timeline;
+
+    private bool hasPlayed = false;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(hasPlayed == false && Vector3.Distance(player.transform.position, transform.position) < 1.0f) {
+            hasPlayed = true;
+            timeline.Play();
+        }
+        //Debug.Log("Trigger distance: " + Vector3.Distance(player.transform.position, transform.position));
+    }
+}
